11ec4e85b23f5901f7f129f766868eadd8bda7d1
EvgenyKashin <kashinevge@gmail.com>
2019-05-16 11:31:57 +0300

    Add new datasets and dataloaders

--- src/vgg.py
+++ src/vgg.py
@@ -2,19 +2,102 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import torchvision
 from torchvision import datasets, models, transforms
-# import matplotlib.pyplot as plt
 import time
 import os
 import copy
 import tqdm
 
-data_dir = 'data'
+import jpeg4py as jpeg
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+
+from albumentations import Compose, HorizontalFlip, SmallestMaxSize, CenterCrop, RandomCrop
+from torchvision import transforms
+from torchvision.transforms import Normalize
+
+data_dir = 'train'
 input_size = 224
-num_classes = 2
-batch_size = 16
+num_classes = 16
+batch_size = 32
 num_epochs = 10
+num_workers = 16
+
+
+def load_train_val_data(data_path):
+    data = Path(data_path)
+
+    paths = list(data.iterdir())
+    labels = [int(p.name.split('.')[0] == 'dog') for p in paths]
+    paths = [str(p) for p in paths]
+
+    df = pd.DataFrame({'path': paths, 'label': labels})
+    df = df.sample(frac=1.0)
+
+    k_fold = StratifiedKFold(n_splits=10, random_state=24)
+    train_ind, val_ind = next(k_fold.split(df.path, df.label))
+
+    train_data = df.iloc[train_ind].reset_index(drop=True)
+    val_data = df.iloc[val_ind].reset_index(drop=True)
+
+    return train_data, val_data
+
+
+IMAGENET_STATS = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+
+
+def to_tensor(x, scale=256):
+    return torch.from_numpy(
+        x.astype(np.float32) / scale).permute(2, 0, 1)
+
+
+resize_size = 256
+img_size = [224, 224]
+
+
+def get_train_augs():
+    return Compose([
+        SmallestMaxSize(resize_size),
+        RandomCrop(*img_size),
+        HorizontalFlip()
+    ])
+
+
+def get_val_augs():
+    return Compose([
+        SmallestMaxSize(resize_size),
+        CenterCrop(*img_size),
+    ])
+
+
+train_augs = get_train_augs()
+val_augs = get_val_augs()
+
+norm_augs = transforms.Compose([
+    to_tensor,
+    Normalize(*IMAGENET_STATS)
+])
+
+
+class CatDogDataset(torch.utils.data.Dataset):
+    def __init__(self, data, additional_transforms):
+        self.data = data
+        self.additional_transforms = additional_transforms
+
+    def __getitem__(self, index):
+        path = self.data.path[index]
+        img = jpeg.JPEG(path).decode()
+        label = self.data.label[index]
+
+        img = self.additional_transforms(image=img)['image']
+        img = norm_augs(img)
+
+        return (img, label)
+
+    def __len__(self):
+        return len(self.data)
 
 
 def train_model(model, dataloaders, criterion, optmizer, num_epochs):
@@ -87,28 +170,19 @@ def create_model(num_cls):
     return model
 
 
-model = create_model(num_classes)
-print(model)
+train, val = load_train_val_data(data_dir)
+train_dataset = CatDogDataset(train, train_augs)
+val_dataset = CatDogDataset(train, val_augs)
 
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(input_size),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+dataloaders_dict = {
+    'train': torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=num_workers),
+    'val': torch.utils.data.DataLoader(val_dataset, batch_size=batch_size,
+                                       shuffle=True, num_workers=num_workers)
 }
 
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in [
-    'train', 'val']}
-dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
-                                                   shuffle=True, num_workers=16) for x in ['train', 'val']}
+model = create_model(num_classes)
+print(model)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
