17ee382a5dbc79164ae18e54e457a131474388ce
EvgenyKashin <kashinevge@gmail.com>
2019-05-22 11:58:42 +0300

    Add results

--- cifar10_train.py
+++ cifar10_train.py
@@ -17,11 +17,11 @@ datasets = cifar10.get_datasets(data_dir)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 if model_name == 'resnet':
-    model = resnet20(num_classes=num_classes)  # 0.877 and 272,474
+    model = resnet20(num_classes=num_classes)  # 0.877 - 272,474
 elif model_name == 'senet':
-    model = se_resnet20(num_classes=num_classes)  # 0.877 and 274,490(+0.7%)
+    model = se_resnet20(num_classes=num_classes)  # 0.877 - 274,490(+0.7%)
 elif model_name == 'srmnet':
-    model = srm_resnet20(num_classes=num_classes)  # and 273,818( +0.5%)
+    model = srm_resnet20(num_classes=num_classes)  # and 0.871 - 273,818( +0.5%)
 
 model = model.to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9,
