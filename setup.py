94b60164cc1fb426809dcde13e747e29b7aa5e8c
evgenykashin <kashinevge@gmail.com>
2020-01-14 16:05:32 +0300

    Update package meta info

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(
     long_description=README,
     long_description_content_type='text/markdown',
     install_requires=INSTALL_PACKAGES,
-    version='0.0.3',
+    version='0.1.3',
     url='https://github.com/EvgenyKashin/backbone',
     author='Evgeny Kashin',
     author_email='kashinevge@gmail.com',
     keywords=['pytorch', 'deep-learning', 'training-loop', 'backbone'],
     license='MIT',
-    python_requires='>=3.6.0'
+    python_requires='>=3.7.0'
 )
